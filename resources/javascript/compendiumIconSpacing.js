996ed403f2c464471a8e7f49ece1f950802dd309
cdavis208 <p14136400@my365.dmu.ac.uk>
2017-01-22 23:45:16 +0000

    compendium index finished, flags setup with nav bar for slider

---
+++ resources/javascript/compendiumIconSpacing.js
@@ -0,0 +1,13 @@
+(function(){
+  var i = 1;
+  var spacingItem = 0+"px";
+
+  var slideIconIndex = document.getElementById("slider-image-"+ i); 
+  
+ 
+  for(i; i < 10; i++){
+    spacingItem - 800;
+    return slideIconIndex;
+  }
+})
+
