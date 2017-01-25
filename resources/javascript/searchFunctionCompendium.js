298d3244d5deafbda19ef17dac7beb3b1a225605
cdavis208 <p14136400@my365.dmu.ac.uk>
2017-01-25 08:54:12 +0000

    javascript using variable to update the submarine name for more information

---
+++ resources/javascript/searchFunctionCompendium.js
@@ -0,0 +1,75 @@
+function myFunction() {
+    var x,text;
+    let url = document.href;
+    var i = 1;
+    // Get the value of the input field with id="search"
+    x = document.getElementById("search").value;
+    let subName = document.getElementById("subClass");
+
+/*
+      if (x = "Germany"){
+        window.alert("Germany has been selected");
+      }
+
+      if (x = "uk"){
+        window.alert("UK has been selected");
+      }
+ */   
+      if (window.location.href.indexOf("slider-image-1") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "UK Typhoon Class Submarine";  
+    }
+
+      if (window.location.href.indexOf("slider-image-2") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "German 212 Class Submarine"; 
+    }
+     if (window.location.href.indexOf("slider-image-3") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "French Triomphant Class Submarine";  
+    }
+
+      if (window.location.href.indexOf("slider-image-4") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "USA SSN-774 Class Submarine"; 
+    }
+     if (window.location.href.indexOf("slider-image-5") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Russian Dolgorukiy Class Submarine";  
+    }
+
+      if (window.location.href.indexOf("slider-image-6") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Japan Soryu Class Submarine"; 
+    }
+     if (window.location.href.indexOf("slider-image-7") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Spanish S-80 Class Submarine";  
+    }
+
+      if (window.location.href.indexOf("slider-image-8") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "North Korean Sang-Yo Class Submarine"; 
+    }
+
+     if (window.location.href.indexOf("slider-image-9") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Chinese Jin Class Submarine"; 
+    }
+   
+  
+     }
+    
+
+
+
+
