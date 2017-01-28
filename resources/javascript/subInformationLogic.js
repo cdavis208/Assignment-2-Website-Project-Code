756215203f30cf63eaf9314e775337c448ccd9fe
cdavis208 <p14136400@my365.dmu.ac.uk>
2017-01-28 23:21:40 +0000

    javascript fixed for timed events
    text now on the image descriptors and compendium 80% finished

---
+++ resources/javascript/subInformationLogic.js
@@ -0,0 +1,99 @@
+let buttonHolder = document.getElementById("button-holder");
+let body = document.getElementById("body");
+let z = 0;
+
+
+      function myFunction() {
+    
+    // Get the value of the input field with id="search"
+    
+    
+      if (window.location.href.indexOf("slider-image-1") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "UK Typhoon Class Submarine";
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+
+       engineinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+
+       operationinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+
+       subinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+
+    }
+
+      if (window.location.href.indexOf("slider-image-2") > -1){
+       /* text = "Valid input";*/
+    
+       subClass.innerHTML = "German 212 Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test 2 this is a test 2 this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+
+       
+    
+    }
+     if (window.location.href.indexOf("slider-image-3") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "French Triomphant Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+    }
+
+      if (window.location.href.indexOf("slider-image-4") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "USA SSN-774 Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+    }
+     if (window.location.href.indexOf("slider-image-5") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Russian Dolgorukiy Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test"  
+    }
+
+      if (window.location.href.indexOf("slider-image-6") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Japan Soryu Class Submarine";
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+    }
+     if (window.location.href.indexOf("slider-image-7") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Spanish S-80 Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test"  
+    }
+
+      if (window.location.href.indexOf("slider-image-8") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "North Korean Sang-Yo Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+    }
+
+     if (window.location.href.indexOf("slider-image-9") > -1){
+       /* text = "Valid input";*/
+      
+       subClass.innerHTML = "Chinese Jin Class Submarine"; 
+
+       wepinfoclass.innerHTML = "this is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a testthis is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test" 
+     }
+ 
+      }
+
+     
+      
+
+      
+
+     
+
+
