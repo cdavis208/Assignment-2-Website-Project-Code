c122f2d956d6f3ec2de3b1f83ea7ff7e36c4a345
cdavis208 <p14136400@my365.dmu.ac.uk>
2017-02-16 02:36:39 +0000

    fixed combo box, son data and dynamically updating span id

--- resources/javascript/aboutPageLogic.js
+++ resources/javascript/aboutPageLogic.js
@@ -1,6 +1,8 @@
-(function(){
+function myFunction(){
 
+  let arr;
   let xmlhttp = new XMLHttpRequest();
+  let aboutTitle = document.getElementById("aboutPageSub");
   
   let url = "https://raw.githubusercontent.com/cdavis208/Assignment-2-Website-Project-Code/master/resources/data/data.json";
 
@@ -8,25 +10,65 @@
     if (this.readyState === 4 && this.status === 200) {
       let myArr = JSON.parse(this.responseText);
       gatherData(myArr);
+      
     }
   };
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 
-  let demoJSON = document.getElementById("sel");
+  
 
   function gatherData(arr) {
-    let theData = "";
-     
+
+    this.arr = arr;
+
+    let feedJSONdata = document.getElementById("sel");
+    let selectedVal = feedJSONdata.selectedIndex;
+   
+    let comboBoxData = "";
     let i;
     
-    for(i = 0; i < arr.length; i++) {
+    
+    for(i = 0; i < arr.aboutPage.length; i++) {
       
-      theData += `<option>${arr.getElementById.title}</option>`;
+      comboBoxData += `<option>${arr.aboutPage[i].title}</option>`;
+      aboutTitle.innerHTML = arr.aboutPage[0].title;
      
+    
     }
-    demoJSON.innerHTML = theData;
-   
+   feedJSONdata.innerHTML = comboBoxData;
   }
 
-}());
+}
+
+function updateAboutTitles() {
+
+ let feedJSONdata = document.getElementById("sel");
+ 
+ let selectedVal = feedJSONdata.selectedIndex;
+    let subTitleData = "";
+    let reason1Data = "";
+    let reason2Data = "";
+    let reason3Data = "";
+    let aboutTitle = document.getElementById("aboutPageSub");
+
+    
+
+   
+     if (selectedVal == 0){
+aboutTitle.innerHTML = arr.aboutPage[0].title;
+
+    } else 
+    if (selectedVal == 1){
+aboutTitle.innerHTML = arr.aboutPage[1].title;
+    } else 
+
+     if (selectedVal == 2){
+aboutTitle.innerHTML = arr.aboutPage[2].title;
+    }
+
+    if (selectedVal == 3){
+aboutTitle.innerHTML = arr.aboutPage[3].title;
+    }
+
+}
