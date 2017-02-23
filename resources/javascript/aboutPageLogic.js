00c6176ccf0bcd2675732febbe65d9951d4387fb
cdavis208 <p14136400@my365.dmu.ac.uk>
2017-02-23 02:22:44 +0000

    General fixes, more content in about page, all new icons in about page, mobile friend submarine compendium and working on large and small desktops

--- resources/javascript/aboutPageLogic.js
+++ resources/javascript/aboutPageLogic.js
@@ -64,12 +64,12 @@ function updateAboutTitles() {
    
      if (selectedVal == 0){
 aboutTitle.innerHTML = arr.aboutPage[0].title;
-box1Text.innerHTML = "sdfdsfsdfsdfdsfdsdf";
-box2Text.innerHTML = "sdfdsfsdfsdfdsfdsdf";
-box3Text.innerHTML = "sdfdsfsdfsdfdsfdsdf";
-box1Img.src="resources/images/aboutPageIcons/jsonicon.jpg"
-box2Img.src="resources/images/aboutPageIcons/dropdownmenu.gif"
-box3Img.src="resources/images/aboutPageIcons/mobilefirsticon.png"
+box1Text.innerHTML = "This web page makes use of JSON data to manipulate the DOM. A drop down box is populated with JSON data parsed as a XMLHTTP request. JSON data is interesting because it can be kept in a form which is not local to the user thus creating securer web applications. I first used JSON data in one of the labs and ever since, I've wanted to incorporate it in someway, so I tested it with comboboxes, allowing this webpage to be possible.";
+box2Text.innerHTML = "A combobox can be found in the top left of this page. This drop down box makes use of the JSON data discussed in the previous section. You can select the choices: about this page, motivations for this website, technologies used, and about me. When selecting another option, the page will be dynamically updated using javascript and sub-categories headers from the JSON data. The combobox will always be defaulted to the first option of the array of items. An event listener checks for onselect through the HTML5.";
+box3Text.innerHTML = "This webpage and website makes use of @media queries designed to make the website mobile, tablet, and desktop friendly. Screen resolutions found commonly on all of these devices have been used. Responsive design for images and text sizes can be found also making using of percentages instead of absolute pixels. Relative screen positioning has been used to ensure screen resizing can hold elements the way they were designed to. This webpage in particular resizes the font and reorganizes the paragraphs to be more mobile friendly.";
+box1Img.src="resources/images/aboutPageIcons/jsonicon.jpg";
+box2Img.src="resources/images/aboutPageIcons/dropdownmenu.gif";
+box3Img.src="resources/images/aboutPageIcons/mobilefirsticon.png";
 
     } else 
     if (selectedVal == 1){
@@ -77,6 +77,9 @@ aboutTitle.innerHTML = arr.aboutPage[1].title;
 box1Text.innerHTML = "zzzzzzzzzzzzzzzzzzzz";
 box2Text.innerHTML = "zzzzzzzzzzzzzzzzzzzz";
 box3Text.innerHTML = "zzzzzzzzzzzzzzzzzzzz";
+box1Img.src="resources/images/aboutPageIcons/bookicon.png";
+box2Img.src="resources/images/aboutPageIcons/futureicon.png";
+box3Img.src="resources/images/aboutPageIcons/passionicon.png";
     } else 
 
      if (selectedVal == 2){
@@ -84,6 +87,9 @@ aboutTitle.innerHTML = arr.aboutPage[2].title;
 box1Text.innerHTML = "vvvvvvvvvvvvvvvvvvvv";
 box2Text.innerHTML = "vvvvvvvvvvvvvvvvvvvv";
 box3Text.innerHTML = "vvvvvvvvvvvvvvvvvvvv";
+box1Img.src="resources/images/aboutPageIcons/html5icon.png";
+box2Img.src="resources/images/aboutPageIcons/css3icon.png";
+box3Img.src="resources/images/aboutPageIcons/jsicon.png";
     }
 
     if (selectedVal == 3){
@@ -91,6 +97,9 @@ aboutTitle.innerHTML = arr.aboutPage[3].title;
 box1Text.innerHTML = "pppppppppppppppppppppp";
 box2Text.innerHTML = "pppppppppppppppppppppp";
 box3Text.innerHTML = "pppppppppppppppppppppp";
+box1Img.src="resources/images/aboutPageIcons/knowledgeicon.png";
+box2Img.src="resources/images/aboutPageIcons/hobbyicon.png";
+box3Img.src="resources/images/aboutPageIcons/courseicon.png";
     }
 
 }
